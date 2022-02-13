44fb81daf5b404a0848a8751aa011ff49cf79559
Niatomi <playervoker@gmail.com>
2022-02-13 15:33:45 +0400

    Initial commit

---
+++ stopLight.ino
@@ -0,0 +1,54 @@
+#define RED 4
+#define GREEN 2
+#define YELLOW 3
+#define BUTTON A0
+
+unsigned int blinkTime = 500;
+unsigned int timeWait = 3000;
+unsigned int remainingTime = 5000;
+
+void setup() {
+
+  pinMode(BUTTON, INPUT);
+
+  pinMode(GREEN, OUTPUT);
+  pinMode(YELLOW, OUTPUT);
+  pinMode(RED, OUTPUT);
+
+  digitalWrite(RED, 0);
+  digitalWrite(GREEN, 1);
+  digitalWrite(YELLOW, 0);
+
+}
+
+void loop() {
+  boolean button = digitalRead(BUTTON);
+  
+  if (button == true ) {
+    
+    for (int i = 4; i != 0; i--) {
+      digitalWrite(GREEN, 0);
+      delay(500);
+      digitalWrite(GREEN, 1);
+      delay(500);
+    }
+    
+    digitalWrite(GREEN, 0);
+    digitalWrite(YELLOW, 1);
+    delay(1000);
+    digitalWrite(YELLOW, 0);
+    delay(1000);
+    digitalWrite(RED, 1);
+    
+    delay(timeWait);
+
+    digitalWrite(YELLOW, 1);
+    delay(1000);
+    digitalWrite(YELLOW, 0);
+    digitalWrite(RED, 0);
+    digitalWrite(GREEN, 1);
+
+    delay(remainingTime);
+  } 
+
+}
